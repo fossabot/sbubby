0b859739ec4fb0515e78eedf5312081a50798b8c
Matthew McAteer <matthewmcateer0@gmail.com>
2019-06-11 12:29:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 """
-Influence Function Class
-Re-implementation of Koh et al., 2018.
+Simple influence calculation runner
+
 :REQUIRES:
 :TODO:
 :AUTHOR: Matthew McAteer
