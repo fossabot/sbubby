ae500f2b517c3e0dc109462c3f1edb79b396deec
Matthew McAteer <matthewmcateer0@gmail.com>
2019-06-18 11:22:27 -0700

    Added additional data feeders

--- sbubby/influence/feeder.py
+++ sbubby/influence/feeder.py
@@ -106,7 +106,6 @@ class InfluenceFeeder:
         """
         raise RuntimeError('must be implemented')
 
-
 class MNISTFeeder(InfluenceFeeder):
     def __init__(self):
         (train_data, train_label), (test_data, test_label) = mnist.load_data() 
@@ -148,6 +147,87 @@ class MNISTFeeder(InfluenceFeeder):
     def reset(self):
         self.train_batch_offset = 0
 
+class CIFAR10Feeder(InfluenceFeeder):
+    def __init__(self):
+        (train_data, train_label), (test_data, test_label) = cifar10.load_data() 
+        num_classes = 10
+        # input image dimensions
+        img_rows, img_cols = 32, 32
+        self.train_origin_data = train_data
+        self.train_origin_label = train_label
+        # load test data
+        self.test_origin_data = test_data
+        self.test_origin_label = test_label
+        
+        if K.image_data_format() == 'channels_first':
+            self.train_data = train_data.reshape(train_data.shape[0], 1, img_rows, img_cols).astype('float32')/255
+            self.test_data = test_data.reshape(test_data.shape[0], 1, img_rows, img_cols).astype('float32')/255
+        else:
+            self.train_data = train_data.reshape(train_data.shape[0], img_rows, img_cols, 1).astype('float32')/255
+            self.test_data = test_data.reshape(test_data.shape[0], img_rows, img_cols, 1).astype('float32')/255
+        
+        self.train_label = keras.utils.to_categorical(train_label, num_classes)
+        self.test_label = keras.utils.to_categorical(test_label, num_classes)
+        
+        self.train_batch_offset = 0
+
+    def test_indices(self, indices):
+        return self.test_data[indices], self.test_label[indices]
+
+    def train_batch(self, batch_size):
+        # calculate offset
+        start = self.train_batch_offset
+        end = start + batch_size
+        self.train_batch_offset += batch_size
+
+        return self.train_data[start:end, ...], self.train_label[start:end, ...]
+
+    def train_one(self, idx):
+        return self.train_data[idx, ...], self.train_label[idx, ...]
+
+    def reset(self):
+        self.train_batch_offset = 0
+
+class CIFAR100Feeder(InfluenceFeeder):
+    def __init__(self):
+        (train_data, train_label), (test_data, test_label) = cifar100.load_data() 
+        num_classes = 10
+        # input image dimensions
+        img_rows, img_cols = 32, 32
+        self.train_origin_data = train_data
+        self.train_origin_label = train_label
+        # load test data
+        self.test_origin_data = test_data
+        self.test_origin_label = test_label
+        
+        if K.image_data_format() == 'channels_first':
+            self.train_data = train_data.reshape(train_data.shape[0], 1, img_rows, img_cols).astype('float32')/255
+            self.test_data = test_data.reshape(test_data.shape[0], 1, img_rows, img_cols).astype('float32')/255
+        else:
+            self.train_data = train_data.reshape(train_data.shape[0], img_rows, img_cols, 1).astype('float32')/255
+            self.test_data = test_data.reshape(test_data.shape[0], img_rows, img_cols, 1).astype('float32')/255
+        
+        self.train_label = keras.utils.to_categorical(train_label, num_classes)
+        self.test_label = keras.utils.to_categorical(test_label, num_classes)
+        
+        self.train_batch_offset = 0
+
+    def test_indices(self, indices):
+        return self.test_data[indices], self.test_label[indices]
+
+    def train_batch(self, batch_size):
+        # calculate offset
+        start = self.train_batch_offset
+        end = start + batch_size
+        self.train_batch_offset += batch_size
+
+        return self.train_data[start:end, ...], self.train_label[start:end, ...]
+
+    def train_one(self, idx):
+        return self.train_data[idx, ...], self.train_label[idx, ...]
+
+    def reset(self):
+        self.train_batch_offset = 0
 
 
 class FashionMNISTFeeder(InfluenceFeeder):
@@ -171,6 +251,40 @@ class FashionMNISTFeeder(InfluenceFeeder):
         
         self.train_label = keras.utils.to_categorical(train_label, num_classes)
         self.test_label = keras.utils.to_categorical(test_label, num_classes)
+        
+        self.train_batch_offset = 0
+
+    def test_indices(self, indices):
+        return self.test_data[indices], self.test_label[indices]
+
+    def train_batch(self, batch_size):
+        # calculate offset
+        start = self.train_batch_offset
+        end = start + batch_size
+        self.train_batch_offset += batch_size
+
+        return self.train_data[start:end, ...], self.train_label[start:end, ...]
+
+    def train_one(self, idx):
+        return self.train_data[idx, ...], self.train_label[idx, ...]
+
+    def reset(self):
+        self.train_batch_offset = 0
+        
+class CustomFeeder(InfluenceFeeder):
+    def __init__(self, input_train_data, input_train_label, input_test_data, input_test_label, num_classes):
+        # input image dimensions
+        self.train_origin_data = input_train_data
+        self.train_origin_label = input_train_label
+        # load test data
+        self.test_origin_data = input_test_data
+        self.test_origin_label = input_test_label
+        
+        self.train_data = input_train_data
+        self.test_data = input_test_data
+        
+        self.train_label = input_train_label
+        self.test_label = input_test_label
         
         self.train_batch_offset = 0
 
