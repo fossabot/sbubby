b4f02857ee9673a06ef07f68ec76f2329fd0d999
Matthew McAteer <matthewmcateer0@gmail.com>
2019-06-27 10:08:38 -0700

    Update __init__.py

--- sbubby/influence/__init__.py
+++ sbubby/influence/__init__.py
@@ -2,5 +2,6 @@ from . import feeder
 from . import hessians
 from . import influence
 from . import plotter
+from . import loss_proxies
 
-__all__ = ["feeder", "hessians", "influence", "plotter"]
+__all__ = ["feeder", "hessians", "influence", "plotter", "loss_proxies"]
